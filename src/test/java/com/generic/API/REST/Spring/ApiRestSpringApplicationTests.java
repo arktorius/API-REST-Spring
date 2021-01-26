bf6affc8760e42ed335883fc368c1d34633a270c
arktorius <lunient@gmail.com>
2021-01-26 23:20:32 +0100

    creating project

---
+++ src/test/java/com/generic/API/REST/Spring/ApiRestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generic.API.REST.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
